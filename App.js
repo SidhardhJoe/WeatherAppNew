f271e8410e38d8cc079748fbbe2cd8170136c446
SidhardhJoe <sidhardhjoe@gmail.com>
2024-07-18 12:09:32 +0530

    Page 1

--- App.js
+++ App.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FirstPage from './src/FirstPage';
+import { useFonts } from 'expo-font';
+
+
+const loadFonts = () => {
+  return useFonts({
+    GilBlack: require("./Fonts/Gilroy-Black.ttf"),
+    GilReg: require("./Fonts/Gilroy-Regular.ttf")
+  })
+}
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName='FirstPage'>
-        <Stack.Screen name="FirstPage" component={FirstPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [Loaded] = loadFonts();
+  if (Loaded) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='FirstPage'>
+          <Stack.Screen name="FirstPage" component={FirstPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
 }
--- src/FirstPage.js
+++ src/FirstPage.js
@@ -1,37 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text, Animated, StatusBar, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Easing } from 'react-native-reanimated';
 
 const FirstPage = () => {
-    const value = useState(new Animated.ValueXY({ x: -400, y: -400 }))[0];
+    const navigation = useNavigation();
+    const value = useState(new Animated.ValueXY({ x: -300, y: -300 }))[0];
+    const value1 = useState(new Animated.ValueXY({ x: -300, y: 0 }))[0];
+    const value2 = useState(new Animated.ValueXY({ x: 0, y: 100 }))[0];
 
     function moveBall() {
         Animated.timing(value, {
             toValue: { x: -60, y: -40 },
-            duration: 1100,
+            duration: 1200,
             useNativeDriver: false,
         }).start();
     }
+    function moveText() {
+        Animated.timing(value1, {
+            toValue: { x: 20, y: 0 },
+            duration: 1200,
+            useNativeDriver: false,
+        }).start();
+    }
+    function moveBox() {
+        Animated.timing(value2, {
+            toValue: { x: -0, y: 0 },
+            duration: 1200,
+            useNativeDriver: false,
+        }).start()
+    }
 
     useEffect(() => {
-        console.log('value', value)
         moveBall();
+        moveText();
+        moveBox();
     }, []);
 
     const size = 350
-    const borderradius = size/2
+    const borderradius = size / 2
 
     return (
         <View style={styles.container}>
             <StatusBar>
-
             </StatusBar>
             <Animated.View style={value.getLayout()}>
-                <View style={[styles.box, {width:size, height:size, borderRadius:borderradius}]}>
-                    <Image source={require("../Images/bg1.png")} style={styles.bg1}/>
+                <View style={[styles.box, { width: size, height: size, borderRadius: borderradius }]}>
+                    <Image source={require("../Images/bg1.png")} style={styles.bg1} />
                 </View>
             </Animated.View>
+            <Animated.View style={value1.getLayout()}>
+                <View>
+                    <Text style={styles.text1}>TRUSTED</Text>
+                    <Text style={styles.text1}>WEATHER</Text>
+                    <Text style={styles.text1}>FORECAST</Text>
+                    <View style={styles.viewsubtext}>
+                        <Text style={styles.text2}>Get to know your weather maps and radar precipitation forecast</Text>
+                    </View>
+                </View>
+            </Animated.View>
+            <View style={styles.lastview}>
+                <Animated.View style={value2.getLayout()}>
+                    <TouchableOpacity style={styles.bottombox}>
+                        <View >
+                            <Text style={styles.lasttxt}>Get Started</Text>
+                        </View>
+                    </TouchableOpacity>
+                </Animated.View>
+            </View>
         </View>
     );
 };
@@ -41,15 +76,43 @@ export default FirstPage;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:"black"
+        backgroundColor: "black",
     },
     box: {
         backgroundColor: '#232323',
-        justifyContent:"center",
-        alignItems:"center"
+        justifyContent: "center",
+        alignItems: "center",
     },
-    bg1:{
-        height:200,
-        width:200
+    bg1: {
+        height: 200,
+        width: 200
+    },
+    text1: {
+        fontFamily: "GilBlack",
+        color: "white",
+        fontSize: 38
+    },
+    text2: {
+        fontFamily: "GilReg",
+        color: "#878787",
+    },
+    viewsubtext: {
+        width: "60%"
+    },
+    bottombox: {
+        height: "45%",
+        width: "90%",
+        backgroundColor: "white",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft:"5%"
+    },
+    lastview: {
+        marginTop: "25%"
+    },
+    lasttxt: {
+        fontFamily: "GilBlack",
+        fontSize: 20
     }
 });
