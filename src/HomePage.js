5d5af89c94f0d4dc1d4c82bcea1c5c872e9c4023
SidhardhJoe <sidhardhjoe@gmail.com>
2024-07-18 17:53:07 +0530

    Display data

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const HomePage = () => {
+
+  const [data, setData]=useState('')
+
+
+  const getData = () =>{
+    axios.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/koratty?unitGroup=metric&include=hours%2Cdays%2Ccurrent%2Calerts&key=F6RQQ5THZ6QFFEP69BKLW8VYX&contentType=json')
+    .then(response =>{
+      setData(response)
+      console.log('res', data.data.currentConditions.temp)
+    }).catch(err =>{
+      console.log('err', err)
+    })
+  }
+
+  useEffect(() => {
+    getData();
+  },[])
+
+
   return (
-    <View>
-      <Text>HomePage</Text>
+    <View style={styles.container}>
+      {data ?<View>
+        <Text style={{ color: "white", padding: 20, fontSize: 18, fontFamily: "GilBlack" }}>City List {data.data.currentConditions.temp}   </Text>
+      </View>:<ActivityIndicator />
+      }
     </View>
   )
 }
 
 export default HomePage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black"
+  }
+})
