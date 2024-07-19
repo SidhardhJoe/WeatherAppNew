c72c386d3a88ba3a2de86d12c094f5c463157cb9
SidhardhJoe <sidhardhjoe@gmail.com>
2024-07-19 13:52:30 +0530

    Loaders

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,41 +1,54 @@
-import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, Image, ActivityIndicator } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Loadingcomponenet from './Loadingcomponenet'
-import {useWindowDimensions} from 'react-native';
+import { useWindowDimensions } from 'react-native';
 
 const HomePage = () => {
 
   const [data, setData] = useState('')
   const WindowwHeight = useWindowDimensions().height
+  const [location, setLocation] = useState('')
+  const [loading, setLoading]= useState(false)
 
 
   const getData = () => {
-    axios.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/koratty?unitGroup=metric&include=hours%2Cdays%2Ccurrent%2Calerts&key=F6RQQ5THZ6QFFEP69BKLW8VYX&contentType=json')
+    setLoading(true)
+    axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=hours%2Cdays%2Ccurrent%2Calerts&key=F6RQQ5THZ6QFFEP69BKLW8VYX&contentType=json`)
       .then(response => {
         setData(response.data);
+        setLoading(false)
         console.log('res', response.data.currentConditions.temp);
       }).catch(err => {
         console.log('err', err);
       });
   };
 
-  useEffect(() => {
-    getData();
-  }, [])
+  // useEffect(() => {
+  //   getData();
+  // }, [])
 
 
   return (
     <ScrollView style={styles.container}>
       <View style={styles.view1}>
         <Text style={styles.citylist}>City List</Text>
-        {data ? <View>
+        {!data ? <Loadingcomponenet />  : <View style={{ height: WindowwHeight * 0.03 }}>
           <Text style={styles.chancefor}>Chance for {data.currentConditions.conditions}</Text>
-        </View> : <Loadingcomponenet />}
+        </View> }
       </View>
-      <View>
-        <TextInput style={[styles.inputbox, {height:WindowwHeight*0.04}]}/>
+      <View style={styles.inputboxview}>
+        <TextInput style={[styles.inputbox, { height: WindowwHeight * 0.05 }]}
+          placeholder='Search for a city or airport'
+          placeholderTextColor="#7C7C7C"
+          value={location}
+          onChangeText={text => setLocation(text)}
+        />
+        <TouchableOpacity style={[styles.searchbox, { height: WindowwHeight * 0.05 }]} onPress={getData }>
+          <Image source={require("../Images/Searchicon.png")} style={styles.icon}/>
+        </TouchableOpacity>
       </View>
+      {loading ? <ActivityIndicator/>:<Text></Text>}
     </ScrollView>
   )
 }
@@ -55,15 +68,34 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontFamily: "GilSemiBold"
   },
-  chancefor:{
-    fontFamily:"GilReg",
-    color:"grey",
-    marginTop:"1%"
+  chancefor: {
+    fontFamily: "GilReg",
+    color: "#7C7C7C",
+    marginTop: "1%"
   },
-  inputbox:{
-    width:"90%",
-    marginHorizontal:"5%",
-    backgroundColor:"white",
-    color:"black"
+  inputbox: {
+    width: "75%",
+    backgroundColor: "#1B1B1B",
+    fontFamily: "GilMed",
+    textAlign: "left",
+    paddingLeft: 10,
+    borderRadius: 5,
+    color: "white"
+  },
+  searchbox: {
+    width: "12%",
+    backgroundColor: "#1B1B1B",
+    marginRight: "3%",
+    borderRadius:5,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  inputboxview: {
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
+  icon:{
+    height:22,
+    width:22
   }
 })
