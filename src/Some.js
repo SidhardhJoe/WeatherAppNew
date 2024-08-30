c37d35397e8fe91997d90e9bb72a6ab323a17221
SidhardhJoe <sidhardhjoe@gmail.com>
2024-08-30 17:27:35 +0530

    got

---
+++ src/Some.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Some = () => {
+  return (
+    <View>
+      <Text>Some</Text>
+    </View>
+  )
+}
+
+export default Some
+
+const styles = StyleSheet.create({})
